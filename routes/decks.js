40593834acf19f30c7c855ae92f87305db6f2195
mollywhitnack <mollywhitnack@gmail.com>
2016-06-25 11:46:25 -0700

    updated

--- routes/decks.js
+++ routes/decks.js
@@ -4,8 +4,8 @@ const express = require('express');
 
 let router = express.Router();
 
+
 router.get('/', (req,res)=>{
-  //let samurai = ['samauri1', 'samauri2','samauri3','samauri4'];
   res.send();
 })
 
